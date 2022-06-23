679dbaf529803ac8bc0a3b44a26a9d5f1443718d
Jay Hutts <janieehutts@gmail.com>
2022-06-23 15:02:41 -0600

    login route completed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
