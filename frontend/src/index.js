33d99fd29d8daef541713401ee4f412b1651a3a5
Jay Hutts <janieehutts@gmail.com>
2022-06-23 16:58:17 -0600

    frontend setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 import './index.css';
 import App from './App';
@@ -10,6 +11,9 @@ import configureStore from './store';
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
