9e3b0d54ff00d466b88cc28ea92700177de80de8
Jay Hutts <janieehutts@gmail.com>
2022-06-23 19:52:07 -0600

    login reducer complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import { restoreCSRF, csrfFetch } from './store/csrf';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 const Root = () => {
