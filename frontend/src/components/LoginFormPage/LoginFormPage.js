a2e66d7c99484fdd534cb8f545f8ecd1ecfe0ed3
Jay Hutts <janieehutts@gmail.com>
2022-06-24 11:35:28 -0600

    authme complete

--- frontend/src/components/LoginFormPage/LoginFormPage.js
+++ frontend/src/components/LoginFormPage/LoginFormPage.js
@@ -1,64 +1,69 @@
-import React, { useState } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 
-import './LoginForm.css';
+// using a modal so this is not necessary.. If I wanted to have the login form
+// on its own page I could do something similar to this
 
-function LoginFormPage() {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const [credential, setCredential] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return (
-    <Redirect to="/" />
-  );
+// import React, { useState } from 'react';
+// import * as sessionActions from '../../store/session';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Redirect } from 'react-router-dom';
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors([]);
-    return dispatch(sessionActions.login({ credential, password }))
-      .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
-      });
-  }
+// import './LoginForm.css';
 
-  return (
-    <>
-      <div className='login-form-page-content'>
-        <h1>Log in</h1>
-        {errors.length ? (
-          <ul className='login-form-errors'>
-            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-          </ul>
-        ) : null}
-        <form className='login-form' onSubmit={handleSubmit}>
-          <label>
-            Username or Email
-            <input
-              type="text"
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              required
-            />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          <button type="submit">Log In</button>
-        </form>
-      </div>
-    </>
-  );
-}
+// function LoginFormPage() {
+//   const dispatch = useDispatch();
+//   const sessionUser = useSelector(state => state.session.user);
+//   const [credential, setCredential] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [errors, setErrors] = useState([]);
 
-export default LoginFormPage;
+//   if (sessionUser) return (
+//     <Redirect to="/" />
+//   );
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setErrors([]);
+//     return dispatch(sessionActions.login({ credential, password }))
+//       .catch(async (res) => {
+//         const data = await res.json();
+//         if (data && data.errors) setErrors(data.errors);
+//       });
+//   }
+
+//   return (
+//     <>
+//       <div className='login-form-page-content'>
+//         <h1>Log in</h1>
+//         {errors.length ? (
+//           <ul className='login-form-errors'>
+//             {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+//           </ul>
+//         ) : null}
+//         <form className='login-form' onSubmit={handleSubmit}>
+//           <label>
+//             Username or Email
+//             <input
+//               type="text"
+//               value={credential}
+//               onChange={(e) => setCredential(e.target.value)}
+//               required
+//             />
+//           </label>
+//           <label>
+//             Password
+//             <input
+//               type="password"
+//               value={password}
+//               onChange={(e) => setPassword(e.target.value)}
+//               required
+//             />
+//           </label>
+//           <button type="submit">Log In</button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default LoginFormPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
 
-import './index.css';
-import App from './App';
-import configureStore from './store';
-import * as sessionActions from './store/session';
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -19,19 +20,21 @@ if (process.env.NODE_ENV !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-const Root = () => {
+function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root/>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
