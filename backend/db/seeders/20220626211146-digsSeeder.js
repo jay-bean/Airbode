e7935b1e7a13476de0231eb6e9689bf165276faa
Jay Hutts <janieehutts@gmail.com>
2022-09-11 17:00:28 -0600

    homepagestyled

--- backend/db/seeders/20220626211146-digsSeeder.js
+++ backend/db/seeders/20220626211146-digsSeeder.js
@@ -2,16 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('Digs', [
       { address:'555 West Road',city:'Tuscon',state:'Arizona', country:'United States', title:'West World', price: 200, description: 'Welcome to West World in Arizona! Bringing you the futuristic and rustic elements of you favorite tv show. We hope you enjoy your stay. ', guests: 6, bedrooms: 5, beds: 7, baths: 3, pets: false, userId: 1, createdAt: new Date(), updatedAt: new Date() },
       { address:'222 Arvada Drive',city:'Steamboat Springs',state:'Colorado', country:'United States', title:'Mountain Retreat', price: 230, description: 'Anytime of the year is the best time of the year to visit Steamboat Springs!', guests: 3, bedrooms: 2, beds: 2, baths: 2, pets: false, userId: 2, createdAt: new Date(), updatedAt: new Date() },
@@ -19,17 +9,16 @@ module.exports = {
       { address:'234 Hundimiento',city:'Limon',state:'Limon', country:'Costa Rica', title:'Paradise Place', price: 300, description: 'Enjoy your stay at Paradise Place! Minutes from the beach with access to our boat. Take a tour of our local jungles, or venture inland to visit one of the many Volcanoes here in Costa Rica. Six bedroom home with terrace and salt water pool located in the back surround by beautiful local trees and flowers. This is a must stay while in Costa Rica! ', guests: 6, bedrooms: 5, beds: 7, baths: 3, pets: false, userId: 2, createdAt: new Date(), updatedAt: new Date() },
       { address:'3423 Rue Petit',city:'Paris',state:'Île-de-France region', country:'France', title:'Villa Hedone', price: 1100, description: 'This home is right in the heart of Paris. The atmosphere! The food! The home boasts vaulted ceilings with marble floors. A bulter and chef will be included with your stay.', guests: 4, bedrooms: 3, beds: 3, baths: 3, pets: true, userId: 2, createdAt: new Date(), updatedAt: new Date() },
       { address:'52 Melbourne Street',city:'Queenstown',state:'South Island', country:'New Zealand', title:'NZ Getaway', price: 230, description: 'Come visit New Zealand in style! This tiny home is just under 600 sqft. with tons of character. The home features a full kitchen and a deck around the back with chairs/hammocks and a grill!', guests: 2, bedrooms: 1, beds: 1, baths: 1, pets: true, userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { address:'5555 West Road',city:'Tuscon',state:'Arizona', country:'United States', title:'East World', price: 200, description: 'Welcome to West World in Arizona! Bringing you the futuristic and rustic elements of you favorite tv show. We hope you enjoy your stay. ', guests: 6, bedrooms: 5, beds: 7, baths: 3, pets: false, userId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { address:'2222 Arvada Drive',city:'Steamboat Springs',state:'Colorado', country:'United States', title:'Mountains Retreat', price: 230, description: 'Anytime of the year is the best time of the year to visit Steamboat Springs!', guests: 3, bedrooms: 2, beds: 2, baths: 2, pets: false, userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { address:'14564 Airondale Way',city:'Miami',state:'Florida', country:'United States', title:`1960's Themed`, price: 400, description: `Take Miami back to the 50's with this gorgeous home! 2 miles from the beach and 5 minutes from downtown Miami.`, guests: 4, bedrooms: 3, beds: 3, baths: 2, pets: true, userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { address:'235 Hundimiento',city:'Limon',state:'Limon', country:'Costa Rica', title:'Paradise Palace', price: 300, description: 'Enjoy your stay at Paradise Place! Minutes from the beach with access to our boat. Take a tour of our local jungles, or venture inland to visit one of the many Volcanoes here in Costa Rica. Six bedroom home with terrace and salt water pool located in the back surround by beautiful local trees and flowers. This is a must stay while in Costa Rica! ', guests: 6, bedrooms: 5, beds: 7, baths: 3, pets: false, userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { address:'3424 Rue Petit',city:'Paris',state:'Île-de-France region', country:'France', title:'Villa Hedonez', price: 1100, description: 'This home is right in the heart of Paris. The atmosphere! The food! The home boasts vaulted ceilings with marble floors. A bulter and chef will be included with your stay.', guests: 4, bedrooms: 3, beds: 3, baths: 3, pets: true, userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { address:'53 Melbourne Street',city:'Queenstown',state:'South Island', country:'New Zealand', title:'New Zealand Getaway', price: 230, description: 'Come visit New Zealand in style! This tiny home is just under 600 sqft. with tons of character. The home features a full kitchen and a deck around the back with chairs/hammocks and a grill!', guests: 2, bedrooms: 1, beds: 1, baths: 1, pets: true, userId: 2, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
     return queryInterface.bulkDelete('Digs', null, {});
   }
 };
--- frontend/src/components/Digs/DigList.js
+++ frontend/src/components/Digs/DigList.js
@@ -5,26 +5,30 @@ function DigList() {
   const digs = useSelector((state) => state.digs);
 
   return (
-    <ul className="digs-home-page">
-      {Object.values(digs).map(dig => {
-        return (
-          <Link className="dig-home-page-link"key={dig.id} to={`/digs/${dig.id}`}>
-            <li className="dig-home-page" dig={dig}>
-              <div className="dig-home-page-image-div">
-              {dig.images && dig.images.length ? <img className="dig-home-page-image" src={`${dig.images[0].url}`}/> : null}
-              </div>
-              <div className="dig-home-flex">
-                <div className="dig-home-text-div">
-                  <div className="dig-home-text">{dig.city}, {dig.state}</div>
-                  <div className="dig-home-text">{dig.country}</div>
-                </div>
-                <div className="dig-home-price">${dig.price}/night</div>
-              </div>
-            </li>
-          </Link>
-        );
-      }).reverse()}
-    </ul>
+    <>
+      <div className="buffer"></div>
+      <div className="digs-home-page">
+        <ul className="digs-home-page-ul">
+          {Object.values(digs).map(dig => {
+            return (
+              <Link className="dig-home-page-link"key={dig.id} to={`/digs/${dig.id}`}>
+                <li className="dig-home-page-li" dig={dig}>
+                  <div className="dig-home-page-image-div">
+                    {dig.images && dig.images.length ? <img className="dig-home-page-image" src={`${dig.images[0].url}`}/> : null}
+                  </div>
+                  <div className="dig-home-flex">
+                    <div className="dig-home-div">
+                      <div className="dig-home-location">{dig.city}, {dig.state}</div>
+                      <div className="dig-home-price"><span className="dig-home-span">${dig.price}</span> night</div>
+                    </div>
+                  </div>
+                </li>
+              </Link>
+            );
+          }).reverse()}
+        </ul>
+      </div>
+    </>
   );
 }
 
