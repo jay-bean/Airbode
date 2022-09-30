cf392509130b5a3ed44f06b8869e9a1aee106999
Jay Hutts <janieehutts@gmail.com>
2022-09-30 11:24:08 -0600

    login/signup modals working correctly

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,18 +1,33 @@
 import React, { useState, useEffect } from "react";
 import * as sessionActions from "../../store/session";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './login.css'
 import SignupFormModal from "../SignupFormPage/SignupFormModal";
 
-function LoginForm({ setShowModalLogin }) {
+function LoginForm({ setShowModal, signupBtn }) {
   const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const [signup, setSignup] = useState(false);
+
+  if (signupBtn) setSignup(true);
+
+  // login info
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
   const [labelActive, setLabelActive] = useState([]);
-  const [showModalSignup, setShowModalSignup] = useState(false);
+  // const [showModalSignup, setShowModalSignup] = useState(false);
 
-  const handleSubmit = (e) => {
+  // signup info
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  // const [errors, setErrors] = useState([]);
+  // const [labelActive, setLabelActive] = useState([]);
+
+  const handleLogin = (e) => {
     e.preventDefault();
     setErrors([]);
     return dispatch(sessionActions.login({ credential, password })).catch(
@@ -23,71 +38,143 @@ function LoginForm({ setShowModalLogin }) {
     );
   };
 
-  // const setShowModalLoginTimeout = () => {
-  //   setShowModalLogin(false);
-
-  //     // setShowModalSignup(true);
-
-  // }
-
-//   useEffect(() => {
-//     setShowModalLoginTimeout();
-//     return () => {
-//       setShowModalSignup(true);
-//     };
-// }, []);
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      setErrors([]);
+      return dispatch(sessionActions.signup({ email, username, password }))
+        .catch(async (res) => {
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors);
+        });
+    }
+    return setErrors(['Passwords must match.']);
+  };
 
   return (
-    <div className="login-modal">
-      <div className="login-title">
-        <p onClick={() => setShowModalLogin(false)} className="cancel"></p>
-        <p className="login-title-p">Log in or sign up</p>
-      </div>
-      <h1 className="login-h1">Welcome to Airbode</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div className="login-container">
-          <div onClick={() => setLabelActive([0])} className="login-divs-trial">
-            <div className='label-div-trial'><label className={labelActive.includes(0) || credential ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Username or Email</label></div>
-            <input
-              className="login-input-trial"
-              type="text"
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              required
-            />
+    <>
+      {!signup ?
+        <div className="login-modal">
+          <div className="login-title">
+            <p onClick={() => setShowModal(false)} className="cancel"></p>
+            <p className="login-title-p">Log in or sign up</p>
           </div>
-          <div onClick={() => setLabelActive([1])} className="login-divs-trial last">
-            <div className='label-div-trial'><label className={labelActive.includes(1) || password ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Password</label></div>
-            <input
-              className="login-input-trial"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+          <h1 className="login-h1">Welcome to Airbode</h1>
+          <form className="login-form" onSubmit={handleLogin}>
+            <div className="login-container">
+              <div onClick={() => setLabelActive([0])} className="login-divs-trial">
+                <div className='label-div-trial'><label className={labelActive.includes(0) || credential ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Username or Email</label></div>
+                <input
+                  className="login-input-trial"
+                  type="text"
+                  value={credential}
+                  onChange={(e) => setCredential(e.target.value)}
+                  required
+                />
+              </div>
+              <div onClick={() => setLabelActive([1])} className="login-divs-trial last">
+                <div className='label-div-trial'><label className={labelActive.includes(1) || password ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Password</label></div>
+                <input
+                  className="login-input-trial"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <p className="email-p">We won't email you at all. This website was made for fun and doesn't actually do anything.</p>
+            <ul className="login-form-errors">
+              {errors.length ? errors.map((error, idx) => (
+                <li className="login-form-errors-li" key={idx}>{error}</li>
+              )) : null}
+            </ul>
+            <button className="login-btn-modal" type="submit">Log In</button>
+          </form>
+          <div>
+            <p className="or">or</p>
+            <p className="line-thru-or"></p>
+            <div className="below-btns-div">
+              {/* <SignupFormModal setShowModalSignup={setShowModalSignup}/> */}
+              <button className="below-btns" type="button" onClick={() => setSignup(true)}>Sign Up</button>
+              <button className="below-btns" type="button"><a className="below-btns-links" href="https://github.com/jay-bean">GitHub</a></button>
+              <button className="below-btns" type="button"><a className="below-btns-links" href="https://www.linkedin.com/in/jay-hutts-300ab9180/">LinkedIn</a></button>
+              <button className="below-btns" type="button"><a className="below-btns-links" href="https://www.jayhutts.dev/">Personal Site</a></button>
+            </div>
           </div>
         </div>
-        <p className="email-p">We won't email you at all. This website was made for fun and doesn't actually do anything.</p>
-        <ul className="login-form-errors">
-          {errors.length ? errors.map((error, idx) => (
-            <li className="login-form-errors-li" key={idx}>{error}</li>
-          )) : null}
-        </ul>
-        <button className="login-btn-modal" type="submit">Log In</button>
-      </form>
-      <div>
-        <p className="or">or</p>
-        <p className="line-thru-or"></p>
-        <div className="below-btns-div">
-          {/* <div onClick={setShowModalLoginTimeout}> */}
-            <SignupFormModal setShowModalSignup={setShowModalSignup}/>
-          {/* </div> */}
-          <button className="below-btns" type="button"><a className="below-btns-links" href="https://github.com/jay-bean">GitHub</a></button>
-          <button className="below-btns" type="button"><a className="below-btns-links" href="https://www.linkedin.com/in/jay-hutts-300ab9180/">LinkedIn</a></button>
-          <button className="below-btns" type="button"><a className="below-btns-links" href="https://www.jayhutts.dev/">Personal Site</a></button>
+
+      :
+
+        <div className="login-modal">
+          <div className="login-title">
+            <p onClick={() => setShowModal(false)} className="cancel"></p>
+            <p className="login-title-p">Log in or sign up</p>
+          </div>
+          <h1 className="login-h1">Welcome to Airbode</h1>
+          <form
+            className="login-form"
+            onSubmit={handleSignup}
+          >
+            <div className="signup-container">
+              <div onClick={() => setLabelActive([0])} className="signup-divs-trial">
+                <div className='label-div-trial'><label className={labelActive.includes(0) || email ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Email</label></div>
+                <input
+                  className="login-input-trial"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div onClick={() => setLabelActive([1])} className="signup-divs-trial last">
+                <div className='label-div-trial'><label className={labelActive.includes(1) || username ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Username</label></div>
+                <input
+                  className="login-input-trial"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div onClick={() => setLabelActive([2])} className="signup-divs-trial last">
+                <div className='label-div-trial'><label className={labelActive.includes(2) || password ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Password</label></div>
+                <input
+                  className="login-input-trial"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div onClick={() => setLabelActive([3])} className="signup-divs-trial last">
+                <div className='label-div-trial'><label className={labelActive.includes(3) || confirmPassword ? "login-label-trial login-label-active-modal-trial" : 'login-label-trial'}>Confirm Password</label></div>
+                <input
+                  className="login-input-trial"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <p className="email-p">We won't email you at all. This website was made for fun and doesn't actually do anything.</p>
+            <ul className="login-form-errors">
+              {errors.map((error, idx) => <li className="login-form-errors-li" key={idx}>{error}</li>)}
+            </ul>
+            <button className="login-btn-modal" type="submit">Sign Up</button>
+          </form>
+          <div className="login-div">
+            <p className="or">Already have an account with us?</p>
+            <p className="line-thru-or"></p>
+            <button className="below-btns login-instead" type="button" onClick={() => setSignup(false)}>Log In</button>
+            {/* <LoginFormModal/> */}
+          </div>
         </div>
-      </div>
-    </div>
+      }
+
+
+    </>
   );
 }
 
--- frontend/src/components/LoginFormModal/LoginFormModal.js
+++ frontend/src/components/LoginFormModal/LoginFormModal.js
@@ -3,15 +3,15 @@ import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
 import '../SignupFormPage/SignupForm.css';
 
-function LoginFormModal() {
-  const [showModalLogin, setShowModalLogin] = useState(false);
+function LoginFormModal({ signupBtn }) {
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className='login-btn-div'>
-      <button className='login-btn' onClick={() => setShowModalLogin(true)}>Log In</button>
-      {showModalLogin && (
-        <Modal onClose={() => setShowModalLogin(false)}>
-          <LoginForm setShowModalLogin={setShowModalLogin}/>
+      {signupBtn ? <button className='nav-signup-btn' type='button' onClick={() => setShowModal(true)}>Sign Up</button> : <button className='login-btn' onClick={() => setShowModal(true)}>Log In</button>}
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm setShowModal={setShowModal} signupBtn={signupBtn}/>
         </Modal>
       )}
     </div>
